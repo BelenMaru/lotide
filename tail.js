5c02f88ba635c9f85ca9847127924c609a8dd734
BelenMaru <bleneee@gmail.com>
2021-08-17 05:33:14 +0000

    add implimented tail

---
+++ tail.js
@@ -0,0 +1,16 @@
+// jshint esversion:6
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+  };
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+// Test Case: Check the original array 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
