47d924e2f18624dbb78c2f0b5c510f47151e9527
BelenMaru <bleneee@gmail.com>
2021-08-17 05:16:13 +0000

    inital implemented head

---
+++ head.js
@@ -0,0 +1,18 @@
+// jshint esversion:6
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+  };
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+const head = function(){
+  return array[0] ?  array [0] : aundefined;
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
