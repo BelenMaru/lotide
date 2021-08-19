747f478795bcdc510f2a6ad638208884281afe8b
BelenMaru <bleneee@gmail.com>
2021-08-19 18:39:03 +0000

    initial key on object commit

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+// jshint esversion:6
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅  Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+  };
+
+const findKeyByValue = function(a, b) { 
+  for (const key in a) {
+    if (a[key] === b) {
+      return key;
+    }
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
