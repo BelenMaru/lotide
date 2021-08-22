1033974a2863ddf9d0c34cfa3fffb9dc9f9fe69c
BelenMaru <bleneee@gmail.com>
2021-08-22 18:10:07 +0000

    edit file

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,5 +1,11 @@
 // jshint esversion:6
 
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅  Assertion passed: ${actual} === ${expected}`);
@@ -7,6 +13,10 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
   }
   };
+
+// Takes in an object and a value. 
+// It should scan the object and return the first key which contains the given value. 
+// If no key with that given value is found, then it should return undefined.
 
 const findKeyByValue = function(a, b) { 
   for (const key in a) {
@@ -16,11 +26,12 @@ const findKeyByValue = function(a, b) {
   }
 };
 
-const bestTVShowsByGenre = { 
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
+
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+// output:
+// ✅  Assertion passed: sci_fi === sci_fi
+// ✅  Assertion passed: drama === drama
+// ✅  Assertion passed: undefined === undefined
