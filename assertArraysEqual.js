1392d39f6f32c35e1c3f621b64842487df435fb3
BelenMaru <bleneee@gmail.com>
2021-08-22 15:22:48 +0000

    updated assertArraysEqual commit

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,29 +1,26 @@
 // jshint esversion:6
 
-const assertArraysEqual = function(array1, array2) {
-  
-if(eqArrays(array1,array2)) {
-  console.log(`✅  Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑  Assertion failed: ${actual} !== ${expected}`);
-  }
-  };
-
-assertArraysEqual([1,2,3],[1,2,3]);
-assertArraysEqual([1,2,3],[3,2,3]);
-
-
 const eqArrays = function (array1, array2) {
-  //return a subset of a given array, removing unwanted elements.
-  const newArr = [];
   if (array1.length !== array2.length) {
     return false;
   }
   for (let i = 0; i < array1.length ; i++) {
     if(array1[i] !== array2[i]) {
-      return false;
+      return false
     }
   }
   return true;
 };
 
+const assertArraysEqual = function(array1, array2) {
+  
+if(eqArrays(array1,array2)) {
+  return eqArrays(array1,array2)
+  console.log(`✅  Assertion passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑  Assertion failed: ${array1} !== ${array2}`);
+  }
+  };
+
+assertArraysEqual([1,2,3],[1,2,3]);
+assertArraysEqual([1,2,3],[3,2,3]);
